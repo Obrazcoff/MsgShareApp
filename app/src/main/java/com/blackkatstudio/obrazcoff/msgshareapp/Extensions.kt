dabd48002a8f99d39ba9a78e93a26ab71059b4f9
Obrazcoff <obrazcoff@gmail.com>
2018-10-27 02:40:27 +0200

    Add default value to function

--- app/src/main/java/com/blackkatstudio/obrazcoff/msgshareapp/Extensions.kt
+++ app/src/main/java/com/blackkatstudio/obrazcoff/msgshareapp/Extensions.kt
@@ -3,6 +3,6 @@ package com.blackkatstudio.obrazcoff.msgshareapp
 import android.content.Context
 import android.widget.Toast
 
-fun Context.showToast(message: String){
-    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT){
+    Toast.makeText(this, message, duration).show()
 }
